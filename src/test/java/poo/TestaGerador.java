e46f31df2557fdca14f8319567b87102a5b53f8d
Emerson Ribeiro de Mello <emersonmello@users.noreply.github.com>
2022-04-20 09:51:51 -0300

    atualizando versão do gradle, junit e shadowJar

--- src/test/java/poo/TestaGerador.java
+++ src/test/java/poo/TestaGerador.java
@@ -1,7 +1,8 @@
 package poo;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestaGerador {
 
@@ -9,39 +10,39 @@ public class TestaGerador {
     @Test
     public void barCode(){
         Principal p = new Principal();
-        Assert.assertTrue(p.gerar("barcode", "12345", "saida.png"));
-        Assert.assertFalse(p.gerar("barcode", "valor invalido", "saida.png"));
+        assertTrue(p.gerar("barcode", "12345", "saida.png"));
+        assertFalse(p.gerar("barcode", "valor invalido", "saida.png"));
     }
 
     @Test
     public void qrCode(){
         Principal p = new Principal();
-        Assert.assertTrue(p.gerar("qrcode", "12345", "saida.png"));
-        Assert.assertTrue(p.gerar("qrcode", "http://www.ifsc.edu.br", "saida.png"));
+        assertTrue(p.gerar("qrcode", "12345", "saida.png"));
+        assertTrue(p.gerar("qrcode", "http://www.ifsc.edu.br", "saida.png"));
     }
 
     @Test
     public void tipoInvalido(){
         Principal p = new Principal();
-        Assert.assertFalse(p.gerar("invalido", "12345", "saida.png"));
-        Assert.assertFalse(p.gerar("", "12345", "saida.png"));
-        Assert.assertFalse(p.gerar(null, "12345", "sainda.png"));
+        assertFalse(p.gerar("invalido", "12345", "saida.png"));
+        assertFalse(p.gerar("", "12345", "saida.png"));
+        assertFalse(p.gerar(null, "12345", "sainda.png"));
     }
 
     @Test
     public void valorVazioOuNulo(){
         Principal p = new Principal();
-        Assert.assertFalse(p.gerar("barcode", "", "saida.png"));
-        Assert.assertFalse(p.gerar("qrcode", "", "saida.png"));
-        Assert.assertFalse(p.gerar("barcode", null, "sainda.png"));
+        assertFalse(p.gerar("barcode", "", "saida.png"));
+        assertFalse(p.gerar("qrcode", "", "saida.png"));
+        assertFalse(p.gerar("barcode", null, "sainda.png"));
     }
 
     @Test
     public void nomeDeArquivoInvalido(){
         Principal p = new Principal();
-        Assert.assertFalse(p.gerar("barcode", "12345", ""));
-        Assert.assertFalse(p.gerar("barcode", "12345", null));
-        Assert.assertFalse(p.gerar("barcode", "12345", "/sempermissao.png"));
-        Assert.assertFalse(p.gerar("qrcode", "12345", "/sempermissao.png"));
+        assertFalse(p.gerar("barcode", "12345", ""));
+        assertFalse(p.gerar("barcode", "12345", null));
+        assertFalse(p.gerar("barcode", "12345", "/sempermissao.png"));
+        assertFalse(p.gerar("qrcode", "12345", "/sempermissao.png"));
     }
 }
